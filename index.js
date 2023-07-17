e098e5a2d966ae226e3ce6ade6f0879093cc5cdd
Subhash <subhashsainani4@gmail.com>
2023-07-17 12:24:44 +1000

    Code modified

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Main Function
 function colorCircle(whiteColor) {
     var changeColor = getRandomColor(whiteColor)
     console.log(changeColor); 
@@ -5,11 +6,12 @@ function colorCircle(whiteColor) {
     element.style = `background-color:${changeColor}`;
 }
 
+// Utility Function
 function getRandomColor(whiteColor){
     var maxVal = whiteColor;
     var randomNumber = Math.random() * maxVal; 
     randomNumber = Math.floor(randomNumber); // conversion from hex to int
-    randomNumber = randomNumber.toString(16); 
+    randomNumber = randomNumber.toString(16); // convert back to hex format
     let randColor = randomNumber.padStart(6, 0); // Padding   
-    return `#${randColor.toUpperCase()}`
+    return `#${randColor.toUpperCase()}` 
 }
